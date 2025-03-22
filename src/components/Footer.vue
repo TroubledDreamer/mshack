020c806aa051660478637b2fbfb50472fb1acde7
TroubledDreamer <carlyonja@gmail.com>
2025-03-22 04:34:00 -0500

    started design

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,16 @@
 <template>
-    <div>footer</div>
+    <div id="footerBackground"></div>
 </template>
+
+<style scoped>
+
+#footerBackground {
+    background-color: #4b4545;
+
+    height: 200px;
+    float: bottom;
+    
+    
+}
+
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,8 +14,40 @@ function goBack() {
 
 <template>
 
-    <div>
-        <button @click="goBack()"><-</button>
-        <h1>header</h1>
+    <div id="headerBackground">
+        <button  id="backButton" @click="goBack()"><-</button>
+  
     </div>
 </template>
+
+
+<style scoped>
+
+#headerBackground {
+    background-color: #4b4545;
+    border: none;
+    display: flex;
+    
+}
+
+
+#backButton {
+    background-color: #121412;
+    border: none;
+    color: white;
+    padding: 20px 28px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+ 
+    cursor: pointer;
+}
+
+
+
+
+
+
+
+</style>
